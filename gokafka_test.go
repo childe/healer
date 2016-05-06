5897428932b1f0fd94e63e62d508cf3f1919801b
childe <heychilde@gmail.com>
2016-05-06 21:45:38 +0800

    test getmetadata

---
+++ gokafka_test.go
@@ -0,0 +1,16 @@
+package gokafka
+
+import "testing"
+
+func TestGetmetadata(t *testing.T) {
+	_, err := GetMetaData("kafka.test:9092", "test", 0, "gokafka")
+	if err != nil {
+		t.Error("could not get metadata of topic(test) from server(kafka.test)")
+		t.Error(err)
+	}
+
+	_, err = GetMetaData("kafka.test:80", "test", 0, "gokafka")
+	if err != nil {
+		t.Log("could not get metadata of topic(test) from 80 port")
+	}
+}
