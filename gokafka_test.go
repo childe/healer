0c73b9ad6aea69bd78791d8755da14731f8c626c
childe <heychilde@gmail.com>
2016-05-11 18:04:14 +0800

    add test case

--- gokafka_test.go
+++ gokafka_test.go
@@ -14,3 +14,16 @@ func TestGetmetadata(t *testing.T) {
 		t.Log("could not get metadata of topic(test) from 80 port")
 	}
 }
+
+func TestGetoffset(t *testing.T) {
+	_, err := GetOffset("kafka.test:9092", "test", 0, 0, "gokafka", -1, 1)
+	if err != nil {
+		t.Error("could not get metadata of topic(test) from server(kafka.test)")
+		t.Error(err)
+	}
+
+	_, err = GetMetaData("kafka.test:80", "test", 0, "gokafka")
+	if err != nil {
+		t.Log("could not get metadata of topic(test) from 80 port")
+	}
+}
