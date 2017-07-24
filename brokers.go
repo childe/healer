62bf874f4651fedee18130aa227f50a1547931a0
childe <heychilde@gmail.com>
2017-07-24 11:06:59 +0800

    get all brokers from metadata requests

--- brokers.go
+++ brokers.go
@@ -1,6 +1,7 @@
 package healer
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -11,14 +12,14 @@ type Brokers struct {
 	brokers []*Broker
 }
 
-func GetBrokers(brokerList string) (*Brokers, error) {
+func NewBrokers(brokerList string) (*Brokers, error) {
 	hasAvailableBroker := false
 	brokers := &Brokers{}
 	brokers.brokers = make([]*Broker, 0)
 	for _, brokerAddr := range strings.Split(brokerList, ",") {
-		broker, err := GetBroker(brokerAddr)
+		broker, err := NewBroker(brokerAddr)
 		if err != nil {
-			glog.Infof("init broker error from %s:%s", brokerAddr, err)
+			glog.Infof("init broker from %s error:%s", brokerAddr, err)
 		} else {
 			brokers.brokers = append(brokers.brokers, broker)
 			hasAvailableBroker = true
@@ -29,6 +30,40 @@ func GetBrokers(brokerList string) (*Brokers, error) {
 		return nil, fmt.Errorf("could not get any available broker from %s", brokerList)
 	}
 
-	// TODO get all brokers
+	// get all brokers
+	metadataResponse, err := brokers.brokers[0].RequestMetaData("")
+	if err != nil {
+		glog.Infof("could not get metadata from %s:%s", brokers.brokers[0].address, err)
+		return brokers, nil
+	}
+
+	if glog.V(10) {
+		s, err := json.MarshalIndent(metadataResponse, "", "  ")
+		if err != nil {
+			glog.Infof("brokers info from metadata: %s", s)
+		} else {
+			glog.Infof("failed to marshal brokers info from metadata: %s", err)
+		}
+	}
+
+	for _, brokerInfo := range metadataResponse.Brokers {
+		brokerAddr := fmt.Fprint("%s:%d", brokerInfo.Host, brokerInfo.Port)
+		broker, err := NewBroker(brokerAddr)
+		if err != nil {
+			glog.Infof("init broker from %s error:%s", brokerAddr, err)
+		} else {
+			brokers.brokers = append(brokers.brokers, broker)
+			break
+		}
+	}
+
+	if glog.V(10) {
+		addresses := make([]string, len(brokers.brokers))
+		for i, broker := range brokers.brokers {
+			addresses[i] = broker.address
+		}
+		glog.Infof("all brokers: %s", strings.Join(addresses))
+	}
+
 	return brokers, nil
 }
