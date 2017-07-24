6d6593201305b74e650decf57e14921d28b64190
childe <heychilde@gmail.com>
2017-07-24 15:55:50 +0800

    add RequestOffset func

--- broker.go
+++ broker.go
@@ -89,3 +89,69 @@ func (broker *Broker) RequestMetaData(topic *string) (*MetadataResponse, error) 
 	//TODO error info in the response
 	return metadataResponse, nil
 }
+
+// GetOffset return the offset values array from server
+func (broker *Broker) RequestOffset(topic *string, partitionID int32, clientID string, timeValue int64, offsets uint32) (*OffsetResponse, error) {
+	correlationID := int32(os.Getpid())
+
+	requestHeader := &RequestHeader{
+		ApiKey:        API_OffsetRequest,
+		ApiVersion:    0,
+		CorrelationId: correlationID,
+		ClientId:      broker.clientID,
+	}
+
+	partitionOffsetRequestInfos := make(map[uint32]*PartitionOffsetRequestInfo)
+	partitionOffsetRequestInfos[uint32(partitionID)] = &PartitionOffsetRequestInfo{
+		Time:               timeValue,
+		MaxNumberOfOffsets: offsets,
+	}
+	topicOffsetRequestInfos := make(map[string]map[uint32]*PartitionOffsetRequestInfo)
+	topicOffsetRequestInfos[*topic] = partitionOffsetRequestInfos
+
+	offsetReqeust := &OffsetReqeust{
+		RequestHeader: requestHeader,
+		ReplicaId:     -1,
+		RequestInfo:   topicOffsetRequestInfos,
+	}
+
+	payload := offsetReqeust.Encode()
+
+	broker.conn.Write(payload)
+
+	responseLengthBuf := make([]byte, 4)
+	_, err := broker.conn.Read(responseLengthBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	responseLength := int(binary.BigEndian.Uint32(responseLengthBuf))
+	responseBuf := make([]byte, 4+responseLength)
+
+	readLength := 0
+	for {
+		length, err := broker.conn.Read(responseBuf[4+readLength:])
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		readLength += length
+		if readLength > responseLength {
+			return nil, errors.New("fetch more data than needed while read getMetaData response")
+		}
+	}
+
+	if readLength != responseLength {
+		return nil, errors.New("do NOT fetch needed length while read getMetaData response")
+	}
+	copy(responseBuf[0:4], responseLengthBuf)
+
+	offsetResponse := &OffsetResponse{}
+	offsetResponse.Decode(responseBuf)
+
+	return offsetResponse, nil
+}
