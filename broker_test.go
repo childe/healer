0d962d1fd630ec94ac9f04a489e3619301195908
childe <heychilde@gmail.com>
2017-07-25 18:18:27 +0800

    new file:   broker_test.go

---
+++ broker_test.go
@@ -0,0 +1,42 @@
+package healer
+
+import (
+	"flag"
+	"testing"
+)
+
+var (
+	brokerAddress = flag.String("broker", "127.0.0.1:9092", "<hostname:port,...,hostname:port> The comma separated list of brokers in the Kafka cluster. (default: 127.0.0.1:9092)")
+	brokers       = flag.String("brokers", "127.0.0.1:9092", "<hostname:port,...,hostname:port> The comma separated list of brokers in the Kafka cluster. (default: 127.0.0.1:9092)")
+)
+
+func TestNewBroker(t *testing.T) {
+	flag.Parse()
+
+	t.Log(*brokerAddress)
+
+	_, err := NewBroker(*brokerAddress, "healer", -1)
+	if err != nil {
+		t.Errorf("new broker from %s error:%s", *brokerAddress, err)
+	}
+
+	_, err = NewBroker(*brokerAddress, "", -1)
+	if err != nil {
+		t.Errorf("new broker from %s error:%s", *brokerAddress, err)
+	}
+
+	_, err = NewBroker(*brokerAddress, "healer", 0)
+	if err != nil {
+		t.Errorf("new broker from %s error:%s", *brokerAddress, err)
+	}
+
+	_, err = NewBroker(*brokerAddress, "", 0)
+	if err != nil {
+		t.Errorf("new broker from %s error:%s", *brokerAddress, err)
+	}
+
+	_, err = NewBroker("127.0.0.1:10000", "", 0)
+	if err == nil {
+		t.Errorf("it should not get new broker from 127.0.0.1:10000")
+	}
+}
