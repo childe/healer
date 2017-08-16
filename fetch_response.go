5c0e6a15728bfd24d9c9cd85011c519a98b962cb
childe <heychilde@gmail.com>
2017-08-16 10:48:20 +0800

    consume payload and emit message streamingly

--- fetch_response.go
+++ fetch_response.go
@@ -69,10 +69,10 @@ func (fetchResponse *FetchResponse) Decode(payload []byte) error {
 	}, responsesCount)
 
 	for i := uint32(0); i < responsesCount; i++ {
-		topicNameLength := uint64(binary.BigEndian.Uint16(payload[offset:]))
+		topicNameLength := binary.BigEndian.Uint16(payload[offset:])
 		offset += 2
-		fetchResponse.Responses[i].TopicName = string(payload[offset : offset+topicNameLength])
-		offset += topicNameLength
+		fetchResponse.Responses[i].TopicName = string(payload[offset : offset+uint64(topicNameLength)])
+		offset += uint64(topicNameLength)
 		glog.V(10).Infof("now process topic %s", fetchResponse.Responses[i].TopicName)
 
 		partitionResponseCount := binary.BigEndian.Uint32(payload[offset:])
@@ -163,20 +163,245 @@ func (fetchResponse *FetchResponse) Decode(payload []byte) error {
 	return nil
 }
 
+func encodeMessageSet(payload []byte, length int, offset int, partition int32, messageSetSizeBytes int32, buffers chan []byte, messages chan *Message) (int, int) {
+	//Offset      int64
+	//MessageSize int32
+
+	////Message
+	//Crc        uint32
+	//MagicByte  int8
+	//Attributes int8
+	//Key        []byte
+	//Value      []byte
+	var messageOffset int64
+	var messageSize int32
+	var originOffset int = offset
+
+	for {
+		for {
+			buffer, more := <-buffers
+			if !more {
+				return offset, length
+			}
+
+			copy(buffer, payload[length:])
+			length += len(buffer)
+
+			if offset+12 < length {
+				continue
+			} else {
+				break
+			}
+		}
+		messageOffset = int64(binary.BigEndian.Uint64(payload[offset:]))
+		glog.V(10).Infof("message offset: %s", messageOffset)
+		offset += 8
+
+		messageSize = int32(binary.BigEndian.Uint32(payload[offset:]))
+		offset += 4
+
+		for {
+			buffer, more := <-buffers
+			if !more {
+				return offset, length
+			}
+
+			copy(buffer, payload[length:])
+			length += len(buffer)
+
+			if offset+int(messageSize) < length {
+				continue
+			} else {
+				break
+			}
+		}
+		message := &Message{}
+		message.Crc = binary.BigEndian.Uint32(payload[offset:])
+		offset += 4
+		message.MagicByte = int8(payload[offset])
+		offset++
+		message.Attributes = int8(payload[offset])
+		offset++
+		keyLength := int(binary.BigEndian.Uint32(payload[offset:]))
+		offset += 4
+		if keyLength == -1 {
+			message.Key = nil
+		} else {
+			message.Key = make([]byte, keyLength)
+			copy(message.Key, payload[offset:offset+keyLength])
+			offset += keyLength
+		}
+
+		valueLength := int(binary.BigEndian.Uint32(payload[offset:]))
+		offset += 4
+		if valueLength == -1 {
+			message.Value = nil
+		} else {
+			message.Value = make([]byte, valueLength)
+			copy(message.Value, payload[offset:offset+valueLength])
+			glog.V(20).Infof("message value: %s", message.Value)
+			offset += valueLength
+		}
+		messages <- message
+
+		if offset-originOffset >= int(messageSetSizeBytes) {
+			break
+		}
+	}
+
+	return offset, length
+}
+
+func encodePartitionResponse(payload []byte, length int, offset int, buffers chan []byte, messages chan *Message) (int, int) {
+	//Partition           int32
+	//ErrorCode           int16
+	//HighwaterMarkOffset int64
+	//MessageSetSizeBytes int32
+	//MessageSet          MessageSet
+	var partition int32 = -1
+	var errorCode int16 = -1
+	var highwaterMarkOffset int64 = -1
+	var messageSetSizeBytes int32 = -1
+
+	for {
+		buffer, more := <-buffers
+		if !more {
+			return offset, length
+		}
+
+		copy(buffer, payload[length:])
+		length += len(buffer)
+
+		if partition == -1 {
+			if offset+4 < length {
+				continue
+			}
+			partition = int32(binary.BigEndian.Uint32(payload[offset:]))
+			offset += 4
+		}
+
+		if errorCode == -1 {
+			if offset+2 < length {
+				continue
+			}
+			errorCode = int16(binary.BigEndian.Uint16(payload[offset:]))
+			offset += 2
+		}
+
+		if highwaterMarkOffset == -1 {
+			if offset+8 < length {
+				continue
+			}
+			highwaterMarkOffset = int64(binary.BigEndian.Uint64(payload[offset:]))
+			offset += 8
+		}
+
+		if messageSetSizeBytes == -1 {
+			if offset+4 < length {
+				continue
+			}
+			messageSetSizeBytes = int32(binary.BigEndian.Uint32(payload[offset:]))
+			offset += 4
+		}
+
+		encodeMessageSet(payload, length, offset, partition, messageSetSizeBytes, buffers, messages)
+	}
+}
+
+func encodePartitionResponses(payload []byte, length int, offset int, buffers chan []byte, messages chan *Message) (int, int) {
+	var partitionResponseCount int = -1
+	var counter int = 0
+
+	for {
+		buffer, more := <-buffers
+		if !more {
+			return offset, length
+		}
+
+		copy(buffer, payload[length:])
+		length += len(buffer)
+
+		if partitionResponseCount == -1 {
+			if offset+4 < length {
+				continue
+			}
+			partitionResponseCount = int(binary.BigEndian.Uint32(payload[offset:]))
+			offset += 4
+
+			if partitionResponseCount == 0 {
+				return offset, length
+			}
+		}
+
+		encodePartitionResponse(payload, length, offset, buffers, messages)
+
+		if counter == partitionResponseCount {
+			return offset, length
+		}
+
+	}
+}
+
+func encodeResponses(payload []byte, length int, buffers chan []byte, messages chan *Message) {
+	var topicName string = ""
+	var topicNameLength int = -1
+
+	var offset int = 12 // processed offset
+
+	for {
+		buffer, more := <-buffers
+		if !more {
+			return
+		}
+
+		copy(buffer, payload[length:])
+		length += len(buffer)
+
+		if topicNameLength == -1 {
+			if offset+2 < length {
+				continue
+			}
+			topicNameLength = int(binary.BigEndian.Uint16(payload[offset:]))
+			offset += 2
+		}
+		if topicName == "" {
+			if offset+topicNameLength < length {
+				continue
+			}
+			topicName = string(payload[offset : offset+topicNameLength])
+			offset += topicNameLength
+		}
+
+		offset, length = encodePartitionResponses(payload, length, offset, buffers, messages)
+
+		topicName = ""
+		topicNameLength = -1
+	}
+}
+
 func consumeFetchResponse(buffers chan []byte, messages chan *Message) {
-	restBuf := make([]byte, 0)
+	var payload []byte
+	length := 0
+	payloadLengthBuf := make([]byte, 0)
+	for {
+		buffer := <-buffers
+		length += len(buffer)
+		payloadLengthBuf := append(payloadLengthBuf, buffer...)
+		if len(payloadLengthBuf) >= 4 {
+			responseLength := binary.BigEndian.Uint32(payloadLengthBuf)
+			payload = make([]byte, responseLength+4)
+			copy(payloadLengthBuf, payload)
+			break
+		}
+	}
 
 	// header
 	for {
-		buf, more := <-buffers
+		buffer, more := <-buffers
 		if more {
-			//responseLength := uint64(binary.BigEndian.Uint32(payload))
-			//fetchResponse.CorrelationId = int32(binary.BigEndian.Uint32(payload[offset:]))
-			//responsesCount := binary.BigEndian.Uint32(payload[offset:])
-			if len(buf) < 12 {
-				restBuf = append(restBuf, buf...)
-			} else {
-				restBuf = restBuf[12:]
+			copy(payloadLengthBuf, payload[length:])
+			length += len(buffer)
+			if length >= 12 {
 				break
 			}
 		} else {
@@ -184,14 +409,10 @@ func consumeFetchResponse(buffers chan []byte, messages chan *Message) {
 			return
 		}
 	}
-	for {
-		_, more := <-buffers
-		if more {
-			//topicNameLength := uint64(binary.BigEndian.Uint16(payload[offset:]))
-			//offset += 2
-			//fetchResponse.Responses[i].TopicName = string(payload[offset : offset+topicNameLength])
-			//offset += topicNameLength
-		} else {
-		}
+
+	responsesCount := binary.BigEndian.Uint32(payload[8:])
+	if responsesCount == 0 {
+		return
 	}
+	encodeResponses(payload, length, buffers, messages)
 }
