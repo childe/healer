a7ec5ed09c4196ae70b0ceee71db4d12af7430ad
childe <heychilde@gmail.com>
2017-11-23 16:40:21 +0800

    NewHeartbeatRequest

--- heartbeat_request.go
+++ heartbeat_request.go
@@ -6,10 +6,25 @@ import (
 
 // TODO version0
 type HeartbeatRequest struct {
-	RequestHeader     *RequestHeader
-	GroupID           string
-	groupGenerationID int32
-	memberID          string
+	RequestHeader *RequestHeader
+	GroupID       string
+	GenerationID  int32
+	MemberID      string
+}
+
+func NewHeartbeatRequest(correlationID uint32, clientID, groupID string, generationID int32, memberID string) *HeartbeatRequest {
+	requestHeader := &RequestHeader{
+		ApiKey:        API_Heartbeat,
+		ApiVersion:    0,
+		CorrelationID: correlationID,
+		ClientId:      clientID,
+	}
+	return &HeartbeatRequest{
+		RequestHeader: requestHeader,
+		GroupID:       groupID,
+		GenerationID:  generationID,
+		MemberID:      memberID,
+	}
 }
 
 func (heartbeatR *HeartbeatRequest) Encode() []byte {
@@ -27,12 +42,12 @@ func (heartbeatR *HeartbeatRequest) Encode() []byte {
 	offset += 2
 	copy(payload[offset:], heartbeatR.GroupID)
 
-	binary.BigEndian.PutUint32(payload[offset:], uint32(heartbeatR.groupGenerationID))
+	binary.BigEndian.PutUint32(payload[offset:], uint32(heartbeatR.GenerationID))
 	offset += 4
 
-	binary.BigEndian.PutUint16(payload[offset:], uint16(len(heartbeatR.memberID)))
+	binary.BigEndian.PutUint16(payload[offset:], uint16(len(heartbeatR.MemberID)))
 	offset += 2
-	copy(payload[offset:], heartbeatR.memberID)
+	copy(payload[offset:], heartbeatR.MemberID)
 
 	return payload
 }
