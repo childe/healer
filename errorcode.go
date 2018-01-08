49c40fb8b52830c8fee1ab7aa24ff35fb42b744e
childe <heychilde@gmail.com>
2018-01-08 15:09:08 +0800

    add 9

--- errorcode.go
+++ errorcode.go
@@ -67,6 +67,11 @@ func init() {
 		ErrorMsg:  "NOT_LEADER_FOR_PARTITION",
 		ErrorDesc: "This server is not the leader for that topic-partition.",
 	}
+	AllError[9] = &Error{
+		Errorcode: 9,
+		ErrorMsg:  "REPLICA_NOT_AVAILABLE",
+		ErrorDesc: "The replica is not available for the requested topic-partition",
+	}
 	AllError[16] = &Error{
 		Errorcode: 16,
 		ErrorMsg:  "NOT_COORDINATOR",
