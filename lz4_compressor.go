b6b44e55c7352329a7081921ce47030502650d4c
childe <heychilde@gmail.com>
2018-02-27 10:51:31 +0800

    use github.com/bkaradzic/go-lz4

--- lz4_compressor.go
+++ lz4_compressor.go
@@ -1,25 +1,10 @@
 package healer
 
-import (
-	"bytes"
-
-	"github.com/pierrec/lz4"
-)
+import "github.com/bkaradzic/go-lz4"
 
 type LZ4Compressor struct {
 }
 
 func (c *LZ4Compressor) Compress(value []byte) ([]byte, error) {
-	var (
-		buf bytes.Buffer
-		err error
-	)
-	writer := lz4.NewWriter(&buf)
-	if _, err = writer.Write(value); err != nil {
-		return nil, err
-	}
-	if err = writer.Close(); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
+	return lz4.Encode(nil, value)
 }
