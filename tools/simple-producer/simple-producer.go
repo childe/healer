e0499f97496137454cfe425fc0161ee5ad4d643e
childe <heychilde@gmail.com>
2018-02-27 10:53:14 +0800

    add compression option

--- tools/simple-producer/simple-producer.go
+++ tools/simple-producer/simple-producer.go
@@ -9,11 +9,12 @@ import (
 )
 
 var (
-	brokers   = flag.String("brokers", "127.0.0.1:9092", "The list of hostname and port of the server to connect to.")
-	topic     = flag.String("topic", "", "REQUIRED: The topic to consume from.")
-	partition = flag.Int("partition", 0, "The partition to consume from.")
-	value     = flag.String("value", "", "")
-	key       = flag.String("key", "", "")
+	brokers         = flag.String("brokers", "127.0.0.1:9092", "The list of hostname and port of the server to connect to.")
+	topic           = flag.String("topic", "", "REQUIRED: The topic to consume from.")
+	partition       = flag.Int("partition", 0, "The partition to consume from.")
+	compressionType = flag.String("compression.type", "none", "defalut:none")
+	value           = flag.String("value", "", "")
+	key             = flag.String("key", "", "")
 )
 
 func main() {
@@ -31,6 +32,7 @@ func main() {
 	config := make(map[string]interface{})
 	config["message.max.count"] = 1
 	config["bootstrap.servers"] = *brokers
+	config["compression.type"] = *compressionType
 	simpleProducer := healer.NewSimpleProducer(*topic, int32(*partition), config)
 
 	if simpleProducer == nil {
