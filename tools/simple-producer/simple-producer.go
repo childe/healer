532a1f29148ddc477f2bf5e4ff50ca08c19f3c97
childe <heychilde@gmail.com>
2018-03-20 15:02:13 +0800

    readline and send

--- tools/simple-producer/simple-producer.go
+++ tools/simple-producer/simple-producer.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/childe/healer"
+	"github.com/golang/glog"
 )
 
 var (
@@ -13,18 +16,12 @@ var (
 	topic           = flag.String("topic", "", "REQUIRED: The topic to consume from.")
 	partition       = flag.Int("partition", 0, "The partition to consume from.")
 	compressionType = flag.String("compression.type", "none", "defalut:none")
-	value           = flag.String("value", "", "")
-	key             = flag.String("key", "", "")
 )
 
 func main() {
 	flag.Parse()
 
 	if *topic == "" {
-		flag.PrintDefaults()
-		os.Exit(4)
-	}
-	if *value == "" {
 		flag.PrintDefaults()
 		os.Exit(4)
 	}
@@ -40,5 +37,31 @@ func main() {
 		os.Exit(5)
 	}
 
-	simpleProducer.AddMessage([]byte(*key), []byte(*value))
+	var (
+		text     []byte = nil
+		line     []byte = nil
+		isPrefix bool   = true
+		err      error  = nil
+	)
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text = nil
+		isPrefix = true
+		for isPrefix {
+			line, isPrefix, err = reader.ReadLine()
+			if err != nil {
+				if err == io.EOF {
+					os.Exit(0)
+				}
+				glog.Errorf("readline error:%s", err)
+				os.Exit(5)
+			}
+			if text == nil {
+				text = line
+			} else {
+				text = append(text, line...)
+			}
+		}
+		simpleProducer.AddMessage([]byte{}, text)
+	}
 }
