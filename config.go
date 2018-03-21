39c61e9ab90667b968b6f6143173f952b1733be9
childe <heychilde@gmail.com>
2018-03-21 18:21:25 +0800

    todo

--- config.go
+++ config.go
@@ -12,10 +12,11 @@ type ProducerConfig struct {
 	FlushIntervalMS          int
 	MetadataMaxAgeMS         int
 	FetchTopicMetaDataRetrys int
-	ConnectionsMaxIdleMS     int
 
-	Retries          int
-	RequestTimeoutMS int32
+	// TODO
+	ConnectionsMaxIdleMS int
+	Retries              int
+	RequestTimeoutMS     int32
 }
 
 var DefaultProducerConfig *ProducerConfig = &ProducerConfig{
