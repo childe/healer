e44e59648739c1275c2c8301fecceb95fee15d5a
childe <heychilde@gmail.com>
2019-01-04 17:53:25 +0800

    assign topic-partitons

--- consumer.go
+++ consumer.go
@@ -5,6 +5,7 @@ import "github.com/golang/glog"
 // Consumer instance is built to consume messages from kafka broker
 type Consumer struct {
 	topic  string
+	assign map[string][]int
 	config *ConsumerConfig
 
 	brokers *Brokers
@@ -17,6 +18,7 @@ func NewConsumer(topic string, config *ConsumerConfig) (*Consumer, error) {
 	c := &Consumer{
 		config: config,
 		topic:  topic,
+		assign: nil,
 	}
 	brokerConfig := getBrokerConfigFromConsumerConfig(config)
 
@@ -28,28 +30,44 @@ func NewConsumer(topic string, config *ConsumerConfig) (*Consumer, error) {
 	return c, nil
 }
 
-func (consumer *Consumer) Consume(fromBeginning bool) (chan *FullMessage, error) {
+func (c *Consumer) Assign(topicPartitons map[string][]int) {
+	c.assign = topicPartitons
+}
+
+func (c *Consumer) Consume(fromBeginning bool) (chan *FullMessage, error) {
 	// get partitions info
 	var (
-		metadataResponse *MetadataResponse
+		metadataResponse *MetadataResponse = nil
 		err              error
 	)
-	for {
-		if metadataResponse, err = consumer.brokers.RequestMetaData(consumer.config.ClientID, []string{consumer.topic}); err != nil {
-			glog.Errorf("could not get metadata of topic %s: %s", consumer.topic, err)
-		} else {
-			break
+
+	if c.assign == nil {
+		for {
+			if metadataResponse, err = c.brokers.RequestMetaData(c.config.ClientID, []string{c.topic}); err != nil {
+				glog.Errorf("could not get metadata of topic %s: %s", c.topic, err)
+			} else {
+				break
+			}
 		}
 	}
 
-	consumer.SimpleConsumers = make([]*SimpleConsumer, 0)
+	c.SimpleConsumers = make([]*SimpleConsumer, 0)
 
-	for _, topicMetadatas := range metadataResponse.TopicMetadatas {
-		topicName := topicMetadatas.TopicName
-		for _, partitionMetadataInfo := range topicMetadatas.PartitionMetadatas {
-			partitionID := partitionMetadataInfo.PartitionID
-			simpleConsumer := NewSimpleConsumerWithBrokers(topicName, partitionID, consumer.config, consumer.brokers)
-			consumer.SimpleConsumers = append(consumer.SimpleConsumers, simpleConsumer)
+	if c.assign == nil {
+		for _, topicMetadatas := range metadataResponse.TopicMetadatas {
+			topicName := topicMetadatas.TopicName
+			for _, partitionMetadataInfo := range topicMetadatas.PartitionMetadatas {
+				partitionID := partitionMetadataInfo.PartitionID
+				simpleConsumer := NewSimpleConsumerWithBrokers(topicName, partitionID, c.config, c.brokers)
+				c.SimpleConsumers = append(c.SimpleConsumers, simpleConsumer)
+			}
+		}
+	} else {
+		for topicName, partitions := range c.assign {
+			for _, p := range partitions {
+				simpleConsumer := NewSimpleConsumerWithBrokers(topicName, int32(p), c.config, c.brokers)
+				c.SimpleConsumers = append(c.SimpleConsumers, simpleConsumer)
+			}
 		}
 	}
 
@@ -61,7 +79,7 @@ func (consumer *Consumer) Consume(fromBeginning bool) (chan *FullMessage, error)
 	}
 
 	messages := make(chan *FullMessage, 10)
-	for _, simpleConsumer := range consumer.SimpleConsumers {
+	for _, simpleConsumer := range c.SimpleConsumers {
 		simpleConsumer.Consume(offset, messages)
 	}
 
