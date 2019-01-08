0a60bc1294ffe7fc4246ec58ed8704f6e19db289
childe <heychilde@gmail.com>
2019-01-08 16:54:02 +0800

    offsets request after sasl

--- test/sasl_handshake_test.go
+++ test/sasl_handshake_test.go
@@ -18,14 +18,17 @@ func init() {
 func TestGenSaslHandShakeRequest(t *testing.T) {
 	var (
 		clientID  string = "healer"
-		mechanism string = ""
+		mechanism string = "PLAIN"
+
+		payload []byte
+		err     error
 	)
 
 	broker, _ := healer.NewBroker(*brokerAddress, -1, healer.DefaultBrokerConfig())
 
 	saslHandShakeRequest := healer.NewSaslHandShakeRequest(clientID, mechanism)
 
-	payload := saslHandShakeRequest.Encode()
+	payload = saslHandShakeRequest.Encode()
 	if len(payload) != 22 {
 		t.Error("SaslHandShakeRequest payload length should be 22")
 	}
@@ -40,4 +43,31 @@ func TestGenSaslHandShakeRequest(t *testing.T) {
 		t.Errorf("decode SaslHandshake response error:%s", err)
 	}
 	t.Logf("SaslHandshake mechanisms: %v", response.EnabledMechanisms)
+
+	// test offsets request
+	var (
+		partitionID int32  = 0
+		timeValue   int64  = 0
+		offsets     uint32 = 10
+		topic       string = "test"
+	)
+
+	offsetsRequest := healer.NewOffsetsRequest(topic, []int32{partitionID}, timeValue, offsets, clientID)
+
+	payload = offsetsRequest.Encode()
+	if len(payload) != 54 {
+		t.Error("offsets request payload length should be 54")
+	}
+	responseBuf, err = broker.Request(offsetsRequest)
+	if err != nil {
+		t.Errorf("requet offsetcommit error:%s", err)
+	}
+
+	t.Logf("%v", responseBuf)
+	offsetsResponse, err := healer.NewOffsetsResponse(responseBuf)
+	if err != nil {
+		t.Errorf("decode offsets response error:%s", err)
+	}
+	t.Logf("%v", offsetsResponse)
+
 }
