87606bae2cb49d77e2a2bf0176d509b85cb4e9f9
childe <heychilde@gmail.com>
2019-04-11 17:03:51 +0800

    rename resourcetype const

--- resource_types.go
+++ resource_types.go
@@ -1,35 +1,11 @@
 package healer
 
 const (
-	UNKNOWN int8 = iota
-
-	/**
-	 * In a filter matches any ResourceType.
-	 */
-	ANY
-
-	/**
-	 * A Kafka topic.
-	 */
-	TOPIC
-
-	/**
-	 * A consumer group.
-	 */
-	GROUP
-
-	/**
-	 * The cluster as a whole.
-	 */
-	CLUSTER
-
-	/**
-	 * A transactional ID.
-	 */
-	TRANSACTIONAL_ID
-
-	/**
-	 * A token ID.
-	 */
-	DELEGATION_TOKEN
+	RESOURCETYPE_UNKNOWN uint8 = iota
+	RESOURCETYPE_ANY
+	RESOURCETYPE_TOPIC
+	RESOURCETYPE_GROUP
+	RESOURCETYPE_CLUSTER
+	RESOURCETYPE_TRANSACTIONAL_ID
+	RESOURCETYPE_DELEGATION_TOKEN
 )
