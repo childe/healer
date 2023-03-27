c4c7c5d630cff2d86d2d9b0b7b3085e282f71a18
childe <rmself@qq.com>
2023-03-27 21:31:37 +0800

    move code from run to RunE

--- command/cmd/simple-consumer.go
+++ command/cmd/simple-consumer.go
@@ -16,66 +16,63 @@ var simpleConsumerCmd = &cobra.Command{
 	Short: "consume message from a certain partition of a topic",
 
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return run(cmd, args)
+		brokers, err := cmd.Flags().GetString("brokers")
+		consumerConfig := map[string]interface{}{"bootstrap.servers": brokers}
+		partition, err := cmd.Flags().GetInt32("partition")
+		topic, err := cmd.Flags().GetString("topic")
+		if topic == "" || err != nil {
+			return errors.New("topic must be specified")
+		}
+		offset, err := cmd.Flags().GetInt64("offset")
+		maxMessages, err := cmd.Flags().GetInt32("max-messages")
+		stopOffset, err := cmd.Flags().GetInt64("stopoffset")
+		printOffset, err := cmd.Flags().GetBool("printoffset")
+		config, err := cmd.Flags().GetString("config")
+
+		for _, kv := range strings.Split(config, ",") {
+			if strings.Trim(kv, " ") == "" {
+				continue
+			}
+			t := strings.SplitN(kv, "=", 2)
+			if len(t) != 2 {
+				return fmt.Errorf("invalid config : %s", kv)
+			}
+			consumerConfig[t[0]] = t[1]
+		}
+		cConfig, err := healer.GetConsumerConfig(consumerConfig)
+		if err != nil {
+			return fmt.Errorf("failed to create config: %w", err)
+		}
+
+		simpleConsumer, err := healer.NewSimpleConsumer(topic, partition, cConfig)
+		if err != nil {
+			return fmt.Errorf("failed to create simple consumer: %w", err)
+		}
+
+		messages, err := simpleConsumer.Consume(offset, nil)
+		if err != nil {
+			glog.Errorf("consumer messages error: %s", err)
+			return err
+		}
+
+		for i := int32(0); i < maxMessages; i++ {
+			message := <-messages
+			if message.Error != nil {
+				fmt.Printf("message error:%s\n", message.Error)
+				break
+			}
+			if stopOffset > 0 && message.Message.Offset >= stopOffset {
+				break
+			}
+			if printOffset {
+				fmt.Printf("%d: %s\n", message.Message.Offset, message.Message.Value)
+			} else {
+				fmt.Printf("%s\n", message.Message.Value)
+			}
+		}
+		return nil
+
 	},
-}
-
-func run(cmd *cobra.Command, args []string) error {
-	brokers, err := cmd.Flags().GetString("brokers")
-	consumerConfig := map[string]interface{}{"bootstrap.servers": brokers}
-	partition, err := cmd.Flags().GetInt32("partition")
-	topic, err := cmd.Flags().GetString("topic")
-	if topic == "" || err != nil {
-		return errors.New("topic must be specified")
-	}
-	offset, err := cmd.Flags().GetInt64("offset")
-	maxMessages, err := cmd.Flags().GetInt32("max-messages")
-	stopOffset, err := cmd.Flags().GetInt64("stopoffset")
-	printOffset, err := cmd.Flags().GetBool("printoffset")
-	config, err := cmd.Flags().GetString("config")
-
-	for _, kv := range strings.Split(config, ",") {
-		if strings.Trim(kv, " ") == "" {
-			continue
-		}
-		t := strings.SplitN(kv, "=", 2)
-		if len(t) != 2 {
-			return fmt.Errorf("invalid config : %s", kv)
-		}
-		consumerConfig[t[0]] = t[1]
-	}
-	cConfig, err := healer.GetConsumerConfig(consumerConfig)
-	if err != nil {
-		return fmt.Errorf("failed to create config: %w", err)
-	}
-
-	simpleConsumer, err := healer.NewSimpleConsumer(topic, partition, cConfig)
-	if err != nil {
-		return fmt.Errorf("failed to create simple consumer: %w", err)
-	}
-
-	messages, err := simpleConsumer.Consume(offset, nil)
-	if err != nil {
-		glog.Errorf("consumer messages error: %s", err)
-		return err
-	}
-
-	for i := int32(0); i < maxMessages; i++ {
-		message := <-messages
-		if message.Error != nil {
-			fmt.Printf("message error:%s\n", message.Error)
-			break
-		}
-		if stopOffset > 0 && message.Message.Offset >= stopOffset {
-			break
-		}
-		if printOffset {
-			fmt.Printf("%d: %s\n", message.Message.Offset, message.Message.Value)
-		} else {
-			fmt.Printf("%s\n", message.Message.Value)
-		}
-	}
-	return nil
 }
 
 func init() {
