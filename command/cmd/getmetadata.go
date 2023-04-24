a303b5e848230d3ccc801341ec9e7c20dc80392d
childe <rmself@qq.com>
2023-04-24 18:44:59 +0800

    cat format

--- command/cmd/getmetadata.go
+++ command/cmd/getmetadata.go
@@ -16,6 +16,7 @@ var getMetadataCmd = &cobra.Command{
 		brorkers, err := cmd.Flags().GetString("brokers")
 		client, err := cmd.Flags().GetString("client")
 		topic, err := cmd.Flags().GetString("topic")
+		format, err := cmd.Flags().GetString("format")
 
 		brokers, err := healer.NewBrokers(brorkers)
 
@@ -34,13 +35,39 @@ var getMetadataCmd = &cobra.Command{
 			return fmt.Errorf("failed to get metadata response: %w", err)
 		}
 
-		s, err := json.MarshalIndent(metadataResponse, "", "  ")
-		if err != nil {
-			return fmt.Errorf("failed to marshal metadata response: %w", err)
+		switch format {
+		case "json":
+			s, err := json.MarshalIndent(metadataResponse, "", "  ")
+			if err != nil {
+				return fmt.Errorf("failed to marshal metadata response: %w", err)
+			}
+			fmt.Println(string(s))
+		case "cat":
+			catResponse(metadataResponse)
 		}
-
-		fmt.Println(string(s))
 
 		return nil
 	},
+}
+
+func catResponse(metadataResponse *healer.MetadataResponse) {
+	fmt.Println("brokers:")
+	bs := make(map[int32]string)
+	for _, b := range metadataResponse.Brokers {
+		fmt.Printf("%d %s:%d\n", b.NodeID, b.Host, b.Port)
+		bs[b.NodeID] = fmt.Sprintf("%s:%d", b.Host, b.Port)
+	}
+
+	fmt.Println("topics:")
+	for _, t := range metadataResponse.TopicMetadatas {
+		topicName := t.TopicName
+		for _, p := range t.PartitionMetadatas {
+			broker := bs[p.Leader]
+			fmt.Printf("%s %d %s\n", topicName, p.PartitionID, broker)
+		}
+	}
+}
+
+func init() {
+	getMetadataCmd.Flags().String("format", "json", "default is json, support json, cat")
 }
