aee0087f029925c5098705f3f45959a662719703
childe <rmself@qq.com>
2023-06-14 15:29:19 +0800

    decode v2 response

--- create_partitions_response.go
+++ create_partitions_response.go
@@ -10,12 +10,14 @@ type CreatePartitionsResponse struct {
 	CorrelationID  uint32                                `json:"correlation_id"`
 	ThrottleTimeMS int32                                 `json:"throttle_time_ms"`
 	Results        []createPartitionsResponseResultBlock `json:"results"`
+	// TAG_BUFFER
 }
 
 type createPartitionsResponseResultBlock struct {
 	TopicName    string `json:"topic_name"`
 	ErrorCode    int16  `json:"error_code"`
 	ErrorMessage string `json:"error_message"`
+	// TAG_BUFFER
 }
 
 // Error implements the error interface, it returns error from error code in the response
@@ -42,31 +44,58 @@ func NewCreatePartitionsResponse(payload []byte, version uint16) (r CreatePartit
 
 	r.CorrelationID = binary.BigEndian.Uint32(payload[offset:])
 	offset += 4
+	if version == 2 {
+		// TAG_BUFFER
+		offset++
+	}
 
 	r.ThrottleTimeMS = int32(binary.BigEndian.Uint32(payload[offset:]))
 	offset += 4
 
-	blockCount := int(binary.BigEndian.Uint32(payload[offset:]))
-	offset += 4
+	var blockCount int
+	if version == 2 {
+		_blockCount, o := compactArrayLength(payload[offset:])
+		offset += o
+		blockCount = int(_blockCount)
+	} else if version == 0 {
+		blockCount = int(binary.BigEndian.Uint32(payload[offset:]))
+		offset += 4
+	}
 
 	r.Results = make([]createPartitionsResponseResultBlock, blockCount)
 	for i := 0; i < blockCount; i++ {
 		r.Results[i] = createPartitionsResponseResultBlock{}
-		l := int(binary.BigEndian.Uint16(payload[offset:]))
-		offset += 2
-		r.Results[i].TopicName = string(payload[offset : offset+l])
-		offset += l
+
+		if version == 2 {
+			name, o := compactString(payload[offset:])
+			offset += o
+			r.Results[i].TopicName = name
+		} else if version == 0 {
+			l := int(binary.BigEndian.Uint16(payload[offset:]))
+			r.Results[i].TopicName = string(payload[offset : offset+l])
+			offset += l + 2
+		}
 
 		r.Results[i].ErrorCode = int16(binary.BigEndian.Uint16(payload[offset:]))
 		offset += 2
 
-		l = int(binary.BigEndian.Uint16(payload[offset:]))
-		offset += 2
-		if l > 0 {
-			r.Results[i].ErrorMessage = string(payload[offset : offset+l])
-			offset += l
+		if version == 2 {
+			msg, o := compactNullableString(payload[offset:])
+			offset += o
+			r.Results[i].ErrorMessage = msg
+		} else if version == 0 {
+			l := int(binary.BigEndian.Uint16(payload[offset:]))
+			offset += 2
+			if l > 0 {
+				r.Results[i].ErrorMessage = string(payload[offset : offset+l])
+				offset += l
+			}
 		}
+		// TAG_BUFFER
+		offset++
 	}
+	// TAG_BUFFER
+	offset++
 
 	return
 }
