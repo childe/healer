a5e5dadf1668ae94ff75318357fc8eefdb6b06c0
childe <rmself@qq.com>
2023-08-12 23:19:01 +0800

    pass post data

--- command/cmd/list-partition-reassignments.go
+++ command/cmd/list-partition-reassignments.go
@@ -13,20 +13,42 @@ var listPartitionReassignmentsCmd = &cobra.Command{
 	Short: "list partition reassignments",
 
 	RunE: func(cmd *cobra.Command, args []string) error {
+		type topicPartition struct {
+			Topic     string `json:"topic"`
+			Partition int32  `json:"partition"`
+		}
+
 		brokers, err := cmd.Flags().GetString("brokers")
 		client, err := cmd.Flags().GetString("client")
 		timeoutMS, err := cmd.Flags().GetInt32("timeout.ms")
-		// topics, err := cmd.Flags().GetStringSlice("topics")
 
 		config := healer.DefaultBrokerConfig()
-		// config.NetConfig.TimeoutMSForEachAPI
+		config.NetConfig.TimeoutMSForEachAPI = make([]int, 68)
+		config.NetConfig.TimeoutMSForEachAPI[healer.API_ListPartitionReassignments] = int(timeoutMS)
 		bs, err := healer.NewBrokersWithConfig(brokers, config)
 
 		if err != nil {
 			return fmt.Errorf("could not create brokers from %s: %w", brokers, err)
 		}
 
+		dataJSONStr, err := cmd.Flags().GetString("data")
+		if err != nil {
+			return err
+		}
+
 		req := healer.NewListPartitionReassignments(client, timeoutMS)
+		if dataJSONStr != "" {
+			topicPartitions := make([]topicPartition, 0)
+			err = json.Unmarshal([]byte(dataJSONStr), &topicPartitions)
+			if err != nil {
+				return err
+			}
+
+			for _, v := range topicPartitions {
+				req.AddTP(v.Topic, v.Partition)
+			}
+		}
+
 		resp, err := bs.ListPartitionReassignments(req)
 
 		if err != nil {
@@ -45,5 +67,5 @@ var listPartitionReassignmentsCmd = &cobra.Command{
 
 func init() {
 	listPartitionReassignmentsCmd.Flags().Int32("timeout.ms", 30000, "The time in ms to wait for the request to complete")
-	listPartitionReassignmentsCmd.Flags().StringSliceP("topics", "t", nil, "comma splited. A list of topics to list partition reassignments for (an empty list will return reassignments for all topics)")
+	listPartitionReassignmentsCmd.Flags().StringP("data", "d", "", `json format data. [{"topic":"test","partition":0},{"topic":"test","partition":2}]`)
 }
