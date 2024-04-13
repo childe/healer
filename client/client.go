6513d59ef526334df30dc4c2fb1f91eb020af745
childe <rmself@qq.com>
2024-04-14 01:58:45 +0800

    add client package

---
+++ client/client.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"github.com/childe/healer"
+	"github.com/go-logr/logr"
+)
+
+type Client struct {
+	clientID string
+
+	logger logr.Logger
+
+	brokers *healer.Brokers
+}
+
+// New creates a new Client.
+func New(logger logr.Logger) *Client {
+	return &Client{}
+}
+
+// Close closes the connections to kafka brokers
+func (c *Client) Close() {
+	c.brokers.Close()
+}
+
+// RefreshMetadata refreshes metadata for c.brokers
+func (c *Client) RefreshMetadata() {
+}
+
+// ListGroups lists all consumer groups from all brokers
+func (c *Client) ListGroups() (groups []string, err error) {
+	for _, brokerinfo := range c.brokers.BrokersInfo() {
+		broker, err := c.brokers.GetBroker(brokerinfo.NodeID)
+		if err != nil {
+			c.logger.Error(err, "get broker failed", "NodeID", brokerinfo.NodeID)
+			return groups, err
+		}
+
+		response, err := broker.RequestListGroups(c.clientID)
+		if err != nil {
+			c.logger.Error(err, "get group list failed", "broker", broker.GetAddress())
+			return groups, err
+		}
+		for _, g := range response.Groups {
+			groups = append(groups, g.GroupID)
+		}
+	}
+	return groups, nil
+}
