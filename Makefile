c0f0b6d35b1716bdde8d051a1e7a335db9d6fa4f
childe <rmself@qq.com>
2024-04-14 21:16:55 +0800

    add more case

--- Makefile
+++ Makefile
@@ -9,6 +9,10 @@ install:
 test:
 	go test -v -gcflags="all=-N -l" .
 
+coverage:
+	go test -v -gcflags="all=-N -l" -coverprofile=coverage.out .
+	go tool cover -html=coverage.out
+
 docker:
 	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o docker/healer ./command
 	docker build -t healer:latest docker
--- broker_test.go
+++ broker_test.go
@@ -38,10 +38,33 @@ func TestNewBroker(t *testing.T) {
 	mockey.PatchConvey("TestNewBroker", t, func() {
 		nodeID := int32(0)
 		addr := "127.0.0.1:9092"
+		errMock := errors.New("NewBroker")
+		mockApiversion := make([]APIVersion, 2)
+		mockApiversion[0] = APIVersion{apiKey: 0, minVersion: 1, maxVersion: 10}
+		mockApiversion[1] = APIVersion{apiKey: 1, minVersion: 1, maxVersion: 10}
+		mockApiversionResponse := APIVersionsResponse{
+			ErrorCode:   0,
+			APIVersions: mockApiversion,
+		}
+
+		mockey.Mock(newConn).Return(nil, nil).Build()
+		mockey.Mock((*Broker).requestAPIVersions).Return(mockApiversionResponse, nil).Build()
+		mockey.Mock((*Broker).sendSaslAuthenticate).Return(errMock).Build()
+
+		config.SaslConfig = &SaslConfig{}
+		_, err := NewBroker(addr, nodeID, config)
+		convey.So(err, convey.ShouldEqual, errMock)
+	})
+
+	mockey.PatchConvey("TestNewBroker", t, func() {
+		nodeID := int32(0)
+		addr := "127.0.0.1:9092"
 
 		mockey.Mock(newConn).Return(nil, nil).Build()
 		mockey.Mock((*Broker).requestAPIVersions).Return(nil, nil).Build()
+		mockey.Mock((*Broker).sendSaslAuthenticate).Return(nil).Build()
 
+		config.SaslConfig = &SaslConfig{}
 		_, err := NewBroker(addr, nodeID, config)
 		convey.So(err, convey.ShouldEqual, nil)
 	})
