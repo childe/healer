0d716de5b341c55c2f8f8472932a615444038b42
childe <rmself@qq.com>
2024-05-26 20:13:34 +0800

    new file:   simple_consumer_test.go

---
+++ simple_consumer_test.go
@@ -0,0 +1,13 @@
+package healer
+
+import (
+	"testing"
+
+	"github.com/bytedance/mockey"
+)
+
+func TestSimpleConsumer(t *testing.T) {
+	mockey.PatchConvey("TestSimpleConsumer", t, func() {
+	})
+
+}
