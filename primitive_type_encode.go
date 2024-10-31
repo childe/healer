e3c907a351d2b5a549f0385eb5dd7964638cb3cd
childe <rmself@qq.com>
2024-10-31 17:55:51 +0800

    Add compact string and bytes en(de)code

--- primitive_type_encode.go
+++ primitive_type_encode.go
@@ -5,16 +5,116 @@ import (
 	"io"
 )
 
-func encodeNullableString(s string) []byte {
-	payload := make([]byte, binary.MaxVarintLen64)
-	offset := binary.PutUvarint(payload, uint64(1+len(s)))
+// https://kafka.apache.org/protocol#protocol_types
+
+// Represents a sequence of characters.
+// First the length N + 1 is given as an UNSIGNED_VARINT .
+// Then N bytes follow which are the UTF-8 encoding of the character sequence.
+func encodeCompactString(s string) []byte {
+	payload := make([]byte, len(s)+binary.MaxVarintLen64)
+	offset := binary.PutUvarint(payload, 1+uint64(len(s)))
 	offset += copy(payload[offset:], s)
 	return payload[:offset]
 }
 
-func writeNullableString(w io.Writer, s string) {
-	payload := make([]byte, binary.MaxVarintLen64)
-	offset := binary.PutUvarint(payload, uint64(1+len(s)))
-	w.Write(payload[:offset])
-	w.Write([]byte(s))
+func writeCompactString(w io.Writer, s string) (n int, err error) {
+	payload := make([]byte, len(s)+binary.MaxVarintLen64)
+	offset := binary.PutUvarint(payload, 1+uint64(len(s)))
+	if n, err = w.Write(payload[:offset]); err != nil {
+		return
+	}
+	o, err := w.Write([]byte(s))
+	return n + o, err
+}
+
+// Represents a sequence of characters or null.
+// For non-null strings, first the length N is given as an INT16.
+// Then N bytes follow which are the UTF-8 encoding of the character sequence.
+// A null value is encoded with length of -1 and there are no following bytes.
+func encodeNullableString(s *string) []byte {
+	if s == nil {
+		return binary.BigEndian.AppendUint16(nil, 0xFFFF)
+	}
+
+	payload := make([]byte, len(*s)+2)
+	binary.BigEndian.PutUint16(payload, uint16(len(*s)))
+	offset := copy(payload[2:], *s)
+	return payload[:2+offset]
+}
+
+func writeNullableString(w io.Writer, s *string) (n int, err error) {
+	if s == nil {
+		return w.Write(binary.BigEndian.AppendUint16(nil, 0xFFFF))
+	}
+	payload := make([]byte, len(*s)+2)
+	binary.BigEndian.PutUint16(payload, uint16(len(*s)))
+	if n, err = w.Write(payload[:2]); err != nil {
+		return
+	}
+	o, err := w.Write([]byte(*s))
+	return n + o, err
+}
+
+// Represents a sequence of characters.
+// First the length N + 1 is given as an UNSIGNED_VARINT .
+// Then N bytes follow which are the UTF-8 encoding of the character sequence.
+// A null string is represented with a length of 0.
+func encodeCompactNullableString(s *string) []byte {
+	if s == nil {
+		return []byte{0}
+	}
+	payload := make([]byte, len(*s)+binary.MaxVarintLen16)
+	offset := binary.PutUvarint(payload, 1+uint64(len(*s)))
+	offset += copy(payload[offset:], *s)
+	return payload[:offset]
+}
+
+func writeCompactNullableString(w io.Writer, s *string) (n int, err error) {
+	if s == nil {
+		return w.Write([]byte{0})
+	}
+	payload := make([]byte, len(*s)+binary.MaxVarintLen16)
+	offset := binary.PutUvarint(payload, 1+uint64(len(*s)))
+	if n, err = w.Write(payload[:offset]); err != nil {
+		return
+	}
+	o, err := w.Write([]byte(*s))
+	return n + o, err
+}
+
+// Represents a raw sequence of bytes.
+// First the length N+1 is given as an UNSIGNED_VARINT.
+// Then N bytes follow.
+func encodeCompactBytes(s []byte) []byte {
+	payload := make([]byte, len(s)+binary.MaxVarintLen32)
+	offset := binary.PutUvarint(payload, 1+uint64(len(s)))
+	offset += copy(payload[offset:], s)
+	return payload[:offset]
+}
+
+// Represents a raw sequence of bytes or null.
+// For non-null values, first the length N is given as an INT32.
+// Then N bytes follow.
+// A null value is encoded with length of -1 and there are no following bytes.
+func encodeNullableBytes(s []byte) []byte {
+	if s == nil {
+		return binary.BigEndian.AppendUint32(nil, 0xFFFFFFFF)
+	}
+	payload := make([]byte, len(s)+4)
+	binary.BigEndian.AppendUint32(payload, uint32(len(s)))
+	offset := copy(payload[4:], s)
+	return payload[:4+offset]
+}
+
+// Represents a raw sequence of bytes.
+// First the length N+1 is given as an UNSIGNED_VARINT.
+// Then N bytes follow. A null object is represented with a length of 0.
+func encodeCompactNullableBytes(s []byte) []byte {
+	if s == nil {
+		return []byte{0}
+	}
+	payload := make([]byte, len(s)+binary.MaxVarintLen32)
+	offset := binary.PutVarint(payload, 1+int64(len(s)))
+	offset += copy(payload[offset:], s)
+	return payload[:offset]
 }
--- primitive_types_decode.go
+++ primitive_types_decode.go
@@ -18,18 +18,31 @@ func compactString(payload []byte) (r string, offset int) {
 	return r, o + int(length)
 }
 
+// Represents a sequence of characters or null.
+// For non-null strings, first the length N is given as an INT16.
+// Then N bytes follow which are the UTF-8 encoding of the character sequence.
+// A null value is encoded with length of -1 and there are no following bytes.
+func nullableString(payload []byte) (r *string, offset int) {
+	length := int16(binary.BigEndian.Uint16(payload))
+	if length == -1 {
+		return nil, 4
+	}
+	s := string(payload[4 : 4+int(length)])
+	return &s, 4 + int(length)
+}
+
 // Represents a sequence of characters.
 // First the length N + 1 is given as an UNSIGNED_VARINT.
 // Then N bytes follow which are the UTF-8 encoding of the character sequence.
 // A null string is represented with a length of 0.
-func compactNullableString(payload []byte) (r string, offset int) {
+func compactNullableString(payload []byte) (r *string, offset int) {
 	length, o := binary.Uvarint(payload)
 	if length == 0 {
-		return "", o
+		return nil, o
 	}
 	length--
-	r = string(payload[o : o+int(length)])
-	return r, o + int(length)
+	s := string(payload[o : o+int(length)])
+	return &s, o + int(length)
 }
 
 // Represents a raw sequence of bytes.
@@ -37,16 +50,45 @@ func compactNullableString(payload []byte) (r string, offset int) {
 // Then N bytes follow.
 func compactBytes(payload []byte) (r []byte, offset int) {
 	length, o := binary.Uvarint(payload)
-	if length == 0 {
-		return nil, o
-	}
 	length--
 	r = payload[o : o+int(length)]
 	return r, o + int(length)
 }
 
+// Represents a raw sequence of bytes or null.
+// For non-null values, first the length N is given as an INT32.
+// Then N bytes follow.
+// A null value is encoded with length of -1 and there are no following bytes.
+func nullableBytes(payload []byte) (r []byte, offset int) {
+	length := int32(binary.BigEndian.Uint32(payload))
+	if length == -1 {
+		return nil, 4
+	}
+	r = payload[4 : 4+int(length)]
+	return r, 4 + int(length)
+}
+
+// Represents a raw sequence of bytes.
+// First the length N+1 is given as an UNSIGNED_VARINT.
+// Then N bytes follow. A null object is represented with a length of 0.
+func compactNullableBytes(payload []byte) (r []byte, offset int) {
+	length, o := binary.Uvarint(payload)
+	length--
+
+	if length == 0 {
+		return nil, o
+	}
+	r = payload[o : o+int(length)]
+	return r, o + int(length)
+}
+
+// Represents a sequence of objects of a given type T.
+// Type T can be either a primitive type (e.g.  STRING) or a structure.
+// First, the length N + 1 is given as an UNSIGNED_VARINT.
+// Then N instances of type T follow.
+// A null array is represented with a length of 0.
+// In protocol documentation an array of T instances is referred to as [T].
 func compactArrayLength(payload []byte) (length uint64, offset int) {
 	length, offset = binary.Uvarint(payload)
-	length--
-	return
+	return length - 1, offset
 }
