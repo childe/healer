aef5f46d8a5dbfc149ef4c3670c07fb99bff742b
childe <rmself@qq.com>
2024-11-11 20:21:06 +0800

    Add mockTaggedFields func and TestTaggedFieldsNil
    generate mocked TaggedFields that used in other test case, and Add
    TestTaggedFieldsNil to test nil TaggedFields

--- tagged_fields_test.go
+++ tagged_fields_test.go
@@ -6,12 +6,33 @@ import (
 	"github.com/smartystreets/goconvey/convey"
 )
 
+func mockTaggedFields(isFlexible bool) TaggedFields {
+	if isFlexible {
+		taggedField := TaggedField{Tag: 1, Data: []byte{1, 2, 3, 4, 5}}
+		return TaggedFields{taggedField, taggedField}
+	}
+	return nil
+}
+
 func TestTaggedFields(t *testing.T) {
 	convey.Convey("Test TaggedFields Encode and Decode", t, func() {
 		taggedFields := TaggedFields{
 			{Tag: 1, Data: []byte{1, 2, 3}},
 			{Tag: 2, Data: []byte{4, 5, 6}},
 		}
+
+		encoded := taggedFields.Encode()
+
+		decoded, length := DecodeTaggedFields(encoded)
+
+		convey.So(length, convey.ShouldEqual, len(encoded))
+		convey.So(decoded, convey.ShouldResemble, taggedFields)
+	})
+}
+
+func TestTaggedFieldsNil(t *testing.T) {
+	convey.Convey("Test nil TaggedFields Encode and Decode", t, func() {
+		var taggedFields TaggedFields = nil
 
 		encoded := taggedFields.Encode()
 
